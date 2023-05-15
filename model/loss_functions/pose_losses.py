a0282c4cd3e3b633ac44bf135db98228f2048205
olayasturias <olaya_93@hotmail.es>
2023-05-15 15:38:57 +0200

    quaternion geodesic loss

--- model/loss_functions/pose_losses.py
+++ model/loss_functions/pose_losses.py
@@ -1,7 +1,46 @@
 import torch
 from torch import nn
 from utils.conversions import rotation_matrix_to_angle_axis , se3_exp_map, so3_exp_map, rotation_matrix_to_quaternion
-from model.metric_functions.vo_metrics import mse_metric,SE3_chordal_metric, SO3_chordal_metric,vector_distance, quaternion_distance_metric
+from model.metric_functions.vo_metrics import mse_metric,SE3_chordal_metric, SO3_chordal_metric,vector_distance, quaternion_distance_metric, quaternion_geodesic_distance
+
+def quaternion_geodesic_loss(data_dict,t_weight = 1, orientation_weight = 100.):
+    ''' Loss function for pose expressed as translation vector and quaternion. Note:
+    data_dict["result"] is a tensor with shape (batch x sequence_len x 7)
+    data_dict["poses"] is a list with len = sequence_len, each element in list
+    is a tensor with shape (batch x (4x4))
+    returns: average for batch and sequence of the loss'''
+    estimate = data_dict["result"] # 1,1,7 - 1,3,7
+    target = data_dict["poses"] # list of seq with (batch, 4x4) 1(1x(4,4)) - 3(1x(4,4))
+
+    seq_len = estimate.size()[1] # (batch, seq, dim_pose)
+
+    loss_dict = dict()
+    sequence_pose_loss = 0.
+    sequence_quaternion_loss = 0.
+    sequence_t_loss = 0.
+    for i in range (0,seq_len): # relative T, we don take first frame in seq
+        # preprocessing target values
+        # preprocessing target values
+        R_target = target[i][:, :3, :3]
+        q_target = rotation_matrix_to_quaternion(R_target)
+        t_target = target[i][:,:-1,-1]
+
+        q_estimate = estimate[:, i, 3:]
+        t_estimate = estimate[:, i, :3]
+
+        quaternion_loss = quaternion_geodesic_distance(q_estimate,q_target)
+        t_loss = vector_distance(t_target,t_estimate)
+        pose_loss = t_weight*t_loss + orientation_weight*quaternion_loss
+        
+        sequence_quaternion_loss += quaternion_loss
+        sequence_t_loss += t_loss
+        sequence_pose_loss += pose_loss
+    loss_dict["loss"] = sequence_pose_loss/seq_len
+    loss_dict["rotation_loss"] = sequence_quaternion_loss/seq_len
+    loss_dict["traslation_loss"] = sequence_t_loss/seq_len
+
+    return loss_dict
+
 
 def quaternion_pose_loss(data_dict,t_weight = 1, orientation_weight = 14.):
     ''' Loss function for pose expressed as translation vector and quaternion. Note:
--- model/metric_functions/vo_metrics.py
+++ model/metric_functions/vo_metrics.py
@@ -2,6 +2,50 @@ import torch
 from torch import nn
 from utils.conversions import rotation_matrix_to_angle_axis, se3_exp_map, so3_exp_map
 
+def qinv(q: torch.Tensor):
+    """
+    Invert quaternions
+    Expect a tensor of shape (*, 4)
+    Returns a tensor of shape (*, 4)
+    """
+    ori_shape = q.shape
+    q = q.contiguous().view(-1, 4)
+    q_norm = torch.norm(q, p=2, dim=1)
+    q_inv = q.clone().contiguous().view(-1, 4)
+    q_inv[:, 1:] *= -1
+    q_inv /= q_norm.unsqueeze(1)
+    return q_inv.reshape(*ori_shape)
+
+
+def quaternion_geodesic_distance(q1,q2):
+    """
+    calculate geodesic distance from q1 to q2 in the form of delta cosine value
+    :param q1: (*, 4)
+    :param q2: (*, 4)
+    :return: (*)
+    """
+    assert q1.shape == q2.shape
+    assert q1.shape[-1] == q2.shape[-1] == 4 or q1.shape[-1] == q2.shape[-1] == 3
+
+    ori_shape = q1.shape
+    q1 = q1.contiguous().view(-1, q1.shape[-1])
+    q2 = q2.contiguous().view(-1, q2.shape[-1])
+
+    if ori_shape[-1] == 4:
+        q1_inv = qinv(q1)
+        terms = torch.bmm(q1_inv.view(-1, 4, 1), q2.view(-1, 1, 4))
+        w = terms[:, 0, 0] - terms[:, 1, 1] - terms[:, 2, 2] - terms[:, 3, 3]
+        w = torch.clamp(w, -1, 1)
+        distance = torch.sub(1, w).view(ori_shape[:-1])
+    else:
+        q1_inv = q1.clone()
+        q1_inv[:, 1:] *= -1
+        terms = q1_inv * q2
+        w = terms[:, 0] - terms[:, 1] - terms[:, 2]
+        w = torch.clamp(w, -1, 1)
+        distance = torch.sub(1, w).view(ori_shape[:-1])
+    return distance
+
 def quaternion_distance_metric(q1, q2):
     """ compute the quaternion distance between two vectors """
     plus_dist = torch.linalg.vector_norm(q1+q2, dim = 1) # norm for each vector in batch
